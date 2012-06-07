533f7bbf84c70a883cca8d8849872f2ae5354ee1
EternalWind <eternalwind@live.cn>
2012-06-07 15:11:35 +0800

    Fixed

--- src/main/stateManager.cpp
+++ src/main/stateManager.cpp
@@ -5,9 +5,6 @@ void NewStateManager::push(StateName stateName)
     dt::State * res = NULL;
     switch ( stateName )
     {
-    case TEST:
-        res = new TestState();
-        break;
     case FIRST_STATE:
         res = new FirstTestState();
         break;
