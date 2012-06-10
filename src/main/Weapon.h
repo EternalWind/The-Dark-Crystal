b972de147a09334521256e1460c5c402da3e4815
xu.jm01@gmail.com <Jianming@Jianming-PC.(none)>
2012-06-10 22:27:30 +0800

    解决冲突

--- src/main/Weapon.h
+++ src/main/Weapon.h
@@ -6,6 +6,7 @@
 #include <Graphics\MeshComponent.hpp>
 #include <Physics\PhysicsBodyComponent.hpp>
 #include <Audio\SoundComponent.hpp>
+#include <Utils\Timer.hpp>
 
 /**
   * 武器类
@@ -32,6 +33,7 @@ public:
 			uint16_t ammo_per_clip, 
 			uint16_t cur_ammo, 
 			bool is_one_shot, 
+			float interval,
 			const QString &firing_sound_handle, 
 			const QString &reloading_begin_sound_handle, 
 			const QString &reloading_done_sound_handle, 
@@ -47,39 +49,45 @@ public:
 
 	void setWeaponType(WeaponType type);
 
-	WeaponType getWeaponType();
+	WeaponType getWeaponType() const;
 
 	void setPower(uint16_t power);
 
-	uint16_t getPower();
+	uint16_t getPower() const;
 
 	void setCurClip(uint16_t cur_clip);
 
-	uint16_t getCurClip();
+	uint16_t getCurClip() const;
 
 	void setMaxClip(uint16_t max_clip);
 
-	uint16_t getMaxClip();
+	uint16_t getMaxClip() const;
 
 	void setWeight(uint16_t weight);
 
-	uint16_t getWeight();
+	uint16_t getWeight() const;
 
 	void setAmmoPerClip(uint16_t ammo_per_clip);
 
-	uint16_t getAmmoPerClip();
+	uint16_t getAmmoPerClip() const;
 
 	void setCurAmmo(uint16_t cur_ammo);
 
-	uint16_t getCurAmmo();
+	uint16_t getCurAmmo() const;
 
 	void setIsOneShot(bool is_one_shot);
 
-	bool getIsOneShot();
+	bool getIsOneShot() const;
 
 	void setHittingRange(float hitting_range);
 
-	float getHittingRange();
+	float getHittingRange() const;
+
+	void setIsPhysicsBodyEnabled(bool is_enabled);
+
+	bool getIsPhysicsBodyEnabled();
+
+	const dt::InteractionComponent* getInterator() const;
 
 	/**
 	  * 射击
@@ -90,6 +98,18 @@ public:
 	  * 装弹
 	  */	
 	void reload();
+
+	void onEnable();
+
+signals:
+	void sAmmoChanged(uint16_t cur_ammo);
+
+	void sClipChanged(uint16_t cur_clip);
+
+private slots:
+	void _onHit(dt::PhysicsBodyComponent *hit);
+
+	void _onReloadCompleted();
 
 private:
 
@@ -103,18 +123,20 @@ private:
 	bool mIsOneShot;							 //!< 是否可连击
 	float mHittingRange;				         //!< 炸弹投掷距离
 
+	bool mIsPhysicsBodyEnabled;					 //!< 是否物理实体
+	dt::Timer* mReloadTimer;					 //!< 控制时间
 	float mInterval;					         //!< 子弹射击间隔
 	float mReloadTime;				             //!< 补充子弹时间
 	QString mMaterialHandle;					 //!< 材质文件名
 	QString mFiringSoundHandle;					 //!< 枪声文件名
 	QString mReloadingBeginSoundHandle;		     //!< 重装弹药开始声音文件名
-	QString mReloadingDoneSoundHandle;		     //!<
-	QString mMeshHandle;					     //!<
-	dt::InteractionComponent* mInteractor;       //!<
-	dt::SoundComponent* mFiringSound;		     //!<
-	dt::SoundComponent* mReloadingBeginSound;    //!<
-	dt::SoundComponent* mReloadingDoneSound;     //!<
-	dt::PhysicsBodyComponent* mPhysicsBody;      //!<
+	QString mReloadingDoneSoundHandle;		     //!< 重装弹药完成声音文件名
+	QString mMeshHandle;					     //!< mesh文件名
+	dt::InteractionComponent* mInteractor;       //!< InteractionComponent指针
+	dt::SoundComponent* mFiringSound;		     //!< 枪声指针
+	dt::SoundComponent* mReloadingBeginSound;    //!< 重装弹药开始声音指针
+	dt::SoundComponent* mReloadingDoneSound;     //!< 重装弹药完成声音指针
+	dt::PhysicsBodyComponent* mPhysicsBody;      //!< PhysicsBodyComponent指针
 };
 
 #endif
