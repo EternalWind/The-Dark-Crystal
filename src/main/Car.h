dbf950225dd0bea7bf3bff0652c46112fc90c40e
Aeron Zhou <334988486@qq.com>
2012-06-12 16:10:57 +0800

    更改各种代码。。。

--- src/main/Car.h
+++ src/main/Car.h
@@ -3,9 +3,63 @@
 
 #include "Vehicle.h"
 
+/**
+  * 游戏载具之一：战车类
+  */
 class Car : public Vehicle {
 public:
 
+	/**
+	  * 构造函数
+	  * @param node_name Node节点名字
+	  * @param mesh_handle 模型句柄
+	  * @param collision_shape_type 物理外形
+	  * @param mass 载具质量
+	  * @param attack_value 载具攻击力
+	  * @param attack_range 载具攻击范围
+	  * @param attack_interval 载具攻击时间间隔
+	  * @param move_sound_handle 战车移动声音句柄
+	  * @param rush_sound_handle 战车加速声音句柄
+	  * @param attack_sound_handle 战车攻击声音句柄
+	  */
+	Car(const QString node_name, 
+		const QString mesh_handle, 
+		const dt::PhysicsBodyComponent::CollisionShapeType collision_shape_type, 
+		const btScalar mass,
+		const uint16_t attack_value,
+		const float attack_range,
+		const float attack_interval,
+		const QString move_sound_handle,
+		const QString rush_sound_handle,
+		const QString attack_sound_handle);
+
+	void onInitialize();
+
+	void onDeinitialize();
+
+protected slots:
+	void __onMove(MoveType type, bool is_pressed);
+
+    void __onAttack(bool is_pressed);
+
+    void __onSpeedUp(bool is_pressed);
+
+    void __onGetOffVehicle();
+
+    void __onLookAround(Ogre::Quaternion quaternion);
+
+	void __onHit(dt::PhysicsBodyComponent* hit);
+
+
+protected:
+	QString mMoveSoundHandle;           //!< 战车移动声音句柄
+	QString mRushSoundHandle;           //!< 战车加速声音句柄
+	QString mAttackSoundHandle;         //!< 战车攻击声音句柄
+	
+	static const QString MOVE_SOUND_COMPONENT;     //!< 战车移动声音Component的名字
+	static const QString RUSH_SOUND_COMPONENT;     //!< 战车加速声音Component的名字
+	static const QString ATTACK_SOUND_COMPONENT;   //!< 战车攻击声音Component的名字
+	static const QString INTERATOR_COMPONENT;      //!< 战车攻击交互Component的名字
 };
 
 #endif
