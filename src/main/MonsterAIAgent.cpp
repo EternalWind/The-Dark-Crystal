91ad69990dd5b60f8c7b071b544ea9cb456a30bc
RaoJi <littlekfc@163.com>
2012-06-12 16:14:10 +0800

    Signed-off-by: RaoJi <littlekfc@163.com>

---
+++ src/main/MonsterAIAgent.cpp
@@ -0,0 +1,2 @@
+#include "MonsterAIAgent.h"
+
--- src/main/MonsterAIAgent.h
+++ src/main/MonsterAIAgent.h
@@ -2,27 +2,29 @@
 #define THE_DARK_CRYSTAL_MONSTERAIAGENT
 
 #include "Agent.h"
+#include "Monster.h"
+
 /**
-  *  注释
+  * Monster的AI实现类
   */
 class MonsterAIAgent : public Agent {
 Q_OBJECT
 public:
 	/**
-  * 注释
-  * @param 参数
-  * @returns 返回值
-  */	
+      * 注释
+      * @param 参数
+      * @returns 返回值
+      */	
 	bool isThreat();
 	void setThreat(bool type); 
-	 /* 当在威胁状态下的时候，寻找一个威胁方位并向它扑过去，在攻击范围的时候就攻击
-  * @param 
-  * @returns 
-  */
+	/** 
+      *当在威胁状态下的时候，寻找一个威胁方位并向它扑过去，在攻击范围的时候就攻击
+      */
 	void findAndAttack();  
 private:
-	
-	bool mThreat;    //!< 是否受到威胁  当周围一定范围内出现对方时，由thickComponent触发	
+
+	Monster * mBody;        //!< 怪物身体
+	bool mThreat;           //!< 是否受到威胁  当周围一定范围内出现对方时，由thickComponent触发	
 
 };
 	
