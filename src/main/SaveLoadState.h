051ad3cc49c8a07c595827732851d643cfff2f02
unknown <SSE@SSE064.(none)>
2012-06-14 10:34:09 +0800

    修改缩进。。

--- src/main/SaveLoadState.h
+++ src/main/SaveLoadState.h
@@ -17,17 +17,17 @@ public:
 	  */
     void onInitialize();
 
-	/**
-	  * 按钮事件响应函数
-	  * @param sender 触发点击事件的按钮
-	  */
+    /**
+      * 按钮事件响应函数
+      * @param sender 触发点击事件的按钮
+      */
 	void onClick(MyGUI::Widget* sender);
 
     /**
-	  * ListBox事件响应函数
-	  * @param sender 触发点击事件的ListBox
+      * ListBox事件响应函数
+      * @param sender 触发点击事件的ListBox
       * @param index 列表的index
-	  */
+      */
     void onListClick(MyGUI::ListBox* sender, size_t index);
 
     void updateStateFrame(double simulation_frame_time);
